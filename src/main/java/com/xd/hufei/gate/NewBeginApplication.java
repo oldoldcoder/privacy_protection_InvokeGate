6fadf98c1deb98d2801602918abd4b759f06984e
oldoldcoder <2742598536@qq.com>
2024-04-13 10:14:22 +0800

    .gitignore重写缓存成功

---
+++ src/main/java/com/xd/hufei/gate/NewBeginApplication.java
@@ -0,0 +1,13 @@
+package com.xd.hufei.newbegin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewBeginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewBeginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xd/hufei/newbegin/NewBeginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xd.hufei.newbegin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewBeginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
